5a2b61e5eccae6620fd47caf3b37c45420eed347
LevantisChris <levantischris@gmail.com>
2023-10-10 09:08:49 +0300

    2-06-10

---
+++ app/src/main/java/com/example/foodapp/Activity/CartListActivity.java
@@ -0,0 +1,16 @@
+package com.example.foodapp.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.foodapp.R;
+
+public class CartListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart_list);
+    }
+}
